15dd04a2293e670e6e6c7e9db8ca932bf73891e8
mochimoko <jerez.solis@gmail.com>
2021-02-10 07:44:01 -0800

    added files

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhVmbKZfl6PXP8Xhi9_mpHxRtX04oOrOg",
+    authDomain: "whatsapp-mern-ec4fe.firebaseapp.com",
+    projectId: "whatsapp-mern-ec4fe",
+    storageBucket: "whatsapp-mern-ec4fe.appspot.com",
+    messagingSenderId: "793557224542",
+    appId: "1:793557224542:web:8f8277a13b94088a8ab8cf"
+  };
+
